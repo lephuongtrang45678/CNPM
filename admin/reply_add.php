56c35c86c298ccb49d11620355904da70754e71f
linhxinhgai <huonglinhlinh095@gmail.com>
2021-11-05 22:33:37 +0700

    ảnh

--- admin/reply_add.php
+++ admin/reply_add.php
@@ -1,19 +1,18 @@
 <?php
+ob_start();
 include("./header.php");
-$bookadid=$_GET['bookadid'];
-$sql="select * from books_add where book_ad_id = $bookadid";
+$bookadid = $_GET['bookadid'];
+$sql = "select * from books_add where book_ad_id = $bookadid";
 $result = mysqli_query($conn, $sql);
-		//bước 3 xử lý kết quả trả về
-		if (mysqli_num_rows($result) > 0) {
-			while ($row = mysqli_fetch_assoc($result)){
-                $book_title=$row['book_title'];
-                $book_author=$row['book_author'];
-                $book_image=$row['book_image'];
-                $book_descr=$row['book_descr'];
-            }
-
-            }
-            else echo $sql;
+//bước 3 xử lý kết quả trả về
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $book_title = $row['book_title'];
+        $book_author = $row['book_author'];
+        $book_image = $row['book_image'];
+        $book_descr = $row['book_descr'];
+    }
+} else echo $sql;
 ?>
 <div class="container-fluid">
     <div class="row mt-5">
@@ -32,31 +31,31 @@ $result = mysqli_query($conn, $sql);
                 </div>
                 <div class="col-md-4">
                     <label for="book_title" class="form-label">Tiêu đề</label>
-                    <input value="<?php echo $book_title?>" type="text" name="book_title" class="form-control" id="book_title" >
+                    <input value="<?php echo $book_title ?>" type="text" name="book_title" class="form-control" id="book_title">
                 </div>
                 <div class="col-md-4">
                     <label for="book_author" class="form-label">Tác giả</label>
-                    <input value="<?php echo $book_author?>" type="text" name="book_author" class="form-control" id="book_author">
+                    <input value="<?php echo $book_author ?>" type="text" name="book_author" class="form-control" id="book_author">
                 </div>
                 <div class="col-md-4">
                     <label for="book_Category" class="form-label">Chủ đề</label>
-                    <input type="text" name="book_Category" class="form-control" id="book_Category" >
+                    <input type="text" name="book_Category" class="form-control" id="book_Category">
                 </div>
                 <div class="col-md-4">
                     <label for="book_descr" class="form-label">Miêu tả về sách</label>
-                    <input value="<?php echo $book_descr?>" type="text" name="book_descr" class="form-control" id="book_descr" >
+                    <input value="<?php echo $book_descr ?>" type="text" name="book_descr" class="form-control" id="book_descr">
                 </div>
                 <div class="col-md-4">
                     <label for="book_price" class="form-label">Giá bán</label>
-                    <input type="text" name="book_price" class="form-control" id="book_price" >
+                    <input type="text" name="book_price" class="form-control" id="book_price">
                 </div>
                 <div class="col-md-4">
                     <label for="publisherid" class="form-label">Nhà sản xuất</label>
-                    <input type="text" name="publisherid" class="form-control" id="publisherid" >
+                    <input type="text" name="publisherid" class="form-control" id="publisherid">
                 </div>
                 <div class="col">
                     <label for="avatar" class="form-label">Thay đổi ảnh</label>
-                    <input value="<?php echo $book_image?>" type="file" id="fileToUpload" name="fileToUpload" accept="images/*" class=" mb-3 form-control" value="chọn ảnh">
+                    <input value="<?php echo $book_image ?>" type="file" id="fileToUpload" name="fileToUpload" accept="images/*" class=" mb-3 form-control" value="chọn ảnh">
                     <div class="preview mb-3">
                         <div id="preview">
                             <img src="#" hidden />
@@ -98,7 +97,7 @@ if (isset($_POST['submit'])) {
     $publisherid1 = $_POST['publisherid'];
 
 
-    $target_dir1 = "img/img-index/"; //chỉ định thư mục nơi tệp sẽ được đặt
+    $target_dir1 = "../img/img-index/"; //chỉ định thư mục nơi tệp sẽ được đặt
     $target_file = $target_dir1 . basename($_FILES["fileToUpload"]["name"]); //chỉ định đường dẫn của tệp sẽ được tải lên
     $uploadOk = 1; //chưa được sử dụng (sẽ được sử dụng sau)
     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION)); //giữ phần mở rộng tệp của tệp 
@@ -143,13 +142,13 @@ if (isset($_POST['submit'])) {
     $res1 = mysqli_query($conn, $sql1);
 
     //4. Check whether the (Query is Executed) data is inserted or not and display appropriate message
-    if ($res1 == TRUE)
-    {
-     $sql2="Delete from books_add where book_ad_id=$bookadid";
-     $res2=mysqli_query($conn,$sql2);
-     if ($res2>0)
-     header("location: reply.php");
-     else echo $sql2;
-    }else echo $sql1;}
+    if ($res1 == TRUE) {
+        $sql2 = "Delete from books_add where book_ad_id=$bookadid"; //tự động xóa theo id người phản hồi
+        $res2 = mysqli_query($conn, $sql2);
+        if ($res2 > 0)
+            header("location: reply.php");
+        else  $sql2;
+    } else  $sql1;
+}
 ?>
 <script type="text/javascript" src="../js/edit-js.js"></script>
